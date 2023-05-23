685f4c849d6b87b983950cd0077efb4dfd95dcc2
whymezi <128997288+whymezi@users.noreply.github.com>
2023-05-23 23:56:39 +0600

    Add files via upload

---
+++ DZ6_zarina_26-03/dz6/app.js
@@ -0,0 +1,20 @@
+function func() {
+    let result;
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        }
+        document.getElementById("result").innerHTML = result;
+    }
